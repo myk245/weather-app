3fa9a6a166e07047ee6507114da4b680167cf5e5
myk245 <myk245@nyu.edu>
2020-10-12 21:45:57 -0400

    display results to ui

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init weather object
 const weather = new Weather('New York'); 
 
+// Init UI object
+const ui = new UI(); 
+
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather); 
 
@@ -9,7 +12,8 @@ document.addEventListener('DOMContentLoaded', getWeather);
 function getWeather() {
    weather.getWeather()
       .then(results => {
-         console.log(results)
+         // console.log(results)
+         ui.paint(results);
       })
-      .catch(error => console.log(error))
+      .catch(error => console.log(error)); 
 }
--- ui.js
+++ ui.js
@@ -2,16 +2,23 @@ class UI {
    constructor() {
       this.location = document.getElementById('w-location');
       this.desc = document.getElementById('w-desc');
-      this.string = document.getElementById('w-string');
+      this.temp = document.getElementById('w-temp');
       this.details = document.getElementById('w-details');
       this.icon = document.getElementById('w-icon');
       this.humidity = document.getElementById('w-humidity');
       this.feelsLike = document.getElementById('w-feels-like');
+      this.precipitation = document.getElementById('w-precipitation');
       this.wind = document.getElementById('w-wind');
    }
 
    paint(weather) {
       this.location.textContent = weather.location.name + ', ' + weather.location.region; 
       this.desc.textContent = weather.current.weather_descriptions[0]; 
+      this.temp.textContent = weather.current.temperature; 
+      this.icon.setAttribute('src', weather.current.weather_icons[0]); 
+      this.humidity.textContent = `Relative Humidity: ${weather.current.humidity}`; 
+      this.feelsLike.textContent = `Feels Like: ${weather.current.feelslike}`; 
+      this.precipitation.textContent = `Chance of Precipitation: ${weather.current.precip}`; 
+      this.wind.textContent = `Wind Speed: ${weather.current.wind_speed}`; 
    }
 }
