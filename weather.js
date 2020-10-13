6de7d674e3ea845eabe2c0df9c0e3ead92fdb96c
myk245 <myk245@nyu.edu>
2020-10-12 21:28:16 -0400

    use fahrenheit in query

--- weather.js
+++ weather.js
@@ -6,7 +6,7 @@ class Weather {
    }
 
    async getWeather() {
-      const response = await fetch(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${this.location}`); 
+      const response = await fetch(`http://api.weatherstack.com/current?units=f&access_key=${apiKey}&query=${this.location}`); 
    
       const responseData = await response.json(); 
       return responseData; 
