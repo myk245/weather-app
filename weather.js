6dfc040f54aa4a2cb65d7135dfd01d75102e670f
myk245 <myk245@nyu.edu>
2020-10-12 20:43:30 -0400

    hide api key

--- weather.js
+++ weather.js
@@ -1,11 +1,11 @@
+const apiKey = config.API_KEY; 
+
 class Weather {
-   constructor(city, state) {
-      this.apiKey = '1f4468c977bea34e30b2f625ae0171fc';
-      this.city = city; 
-      this.state = state; 
+   constructor(location) {
+      this.location = location; 
    }
 
    async getWeather() {
-      const response = await fetch(`http://api.weatherstack.com/`)
+      const response = await fetch(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${this.location}`)
    }
 }
