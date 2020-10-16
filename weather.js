beb3371ead64c4f237c5e41f937744cbde192aaf
myk245 <myk245@nyu.edu>
2020-10-16 18:26:29 -0400

    changes to allow for hosting on github pages

--- weather.js
+++ weather.js
@@ -1,12 +1,13 @@
-const apiKey = config.API_KEY; 
+// const apiKey = config.API_KEY; 
 
 class Weather {
    constructor(location) {
+      this.apiKey = '1f4468c977bea34e30b2f625ae0171fc'; 
       this.location = location; 
    }
 
    async getWeather() {
-      const response = await fetch(`http://api.weatherstack.com/current?units=f&access_key=${apiKey}&query=${this.location}`); 
+      const response = await fetch(`http://api.weatherstack.com/current?units=f&access_key=${this.apiKey}&query=${this.location}`); 
    
       const responseData = await response.json(); 
       return responseData; 
